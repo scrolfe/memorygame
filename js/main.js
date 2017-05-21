bd7eb91199de29fdcc33b565f3519cb14556f4a0
Sam Rolfe <scrolfe@gmail.com>
2017-05-21 15:27:54 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -22,9 +22,12 @@ var cards = [
 		cardImage: "images/king-of-diamonds.png"
 	}
 ];
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
+	// Error \/
+	this.setAttribute('src', cards[0].cardImage);
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a match!");
@@ -32,12 +35,23 @@ var checkForMatch = function() {
 			alert("Sorry, try again.");
 }}};
 
-var flipCard = function(cardID) {
-	console.log("User flipped " + cards[cardID].rank + ".");
-	cardsInPlay.push(cards[cardID].rank);
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	console.log("User flipped " + cards[cardId].rank + ".");
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
 };
-flipCard(0);
-flipCard(1);
-console.log(cards[0].cardImage);
-console.log(cards[1].suit);
+
+var createBoard = function () {
+	for (i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+
+
+};
+
+createBoard();
