c1d16a0c6bafb451399875d1537e69927c02d76c
Sam Rolfe <scrolfe@gmail.com>
2017-05-21 00:49:24 -0500

    Create game logic

--- js/main.js
+++ js/main.js
@@ -2,18 +2,21 @@ console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[1];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
 
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else { 
-		alert("Sorry, try again.");
-// It seems an 'if' statement does not necessitate an 'else' clause.
-}}
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");
+}}};
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var flipCard = function(cardID) {
+	console.log("User flipped " + cards[cardID] + ".");;
+	cardsInPlay.push(cards[cardID]);
+	checkForMatch();
+};
+flipCard(0);
+flipCard(2);
+
+//
